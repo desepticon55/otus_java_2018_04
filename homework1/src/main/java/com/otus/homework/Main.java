9376c410496022c179918710b87d98e731fe6e11
Alexey Bodyak <desepticon555@gmail.com>
2018-04-19 23:18:56 +0300

    added injar and outjar in proguard config

--- homework1/src/main/java/com/otus/homework/Main.java
+++ homework1/src/main/java/com/otus/homework/Main.java
@@ -2,6 +2,10 @@ package com.otus.homework;
 
 import com.google.common.base.Joiner;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
     String s = Joiner.on(",").join(1, 2, 3);
@@ -10,6 +14,36 @@ public class Main {
 
     Pair<Integer, String> pair = Pair.of(1, "Hello World" );
     System.out.println(pair);
+
+    getMiddleInLinkedList();
   }
 
+  private static void getMiddleInLinkedList() {
+    List<String> linkedList = new LinkedList<>();
+    linkedList.add("1");
+    linkedList.add("2");
+    linkedList.add("3");
+    linkedList.add("4");
+    linkedList.add("5");
+    linkedList.add("6");
+    linkedList.add("7");
+    linkedList.add("8");
+    linkedList.add("9");
+    linkedList.add("10");
+    Iterator<String> iterator = linkedList.iterator();
+    Iterator<String> iterator1 = linkedList.iterator();
+    int i = 0;
+    String middle = null;
+    while (iterator1.hasNext()) {
+      if (i % 2 == 0) {
+        middle = iterator.next();
+      }
+      i++;
+      if (iterator1.hasNext()) {
+        iterator1.next();
+      }
+    }
+    System.out.println("length of LinkedList: " + linkedList.size());
+    System.out.println("middle element of LinkedList : " + middle);
+  }
 }
